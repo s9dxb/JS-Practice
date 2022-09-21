738d98490f1564c3dd77e74d731f9c4db29d8e1f
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2022-09-21 10:57:18 +0200

    first test

---
+++ script.js
@@ -0,0 +1,45 @@
+const names = ["Thor", "Wolverine", "Venom"];
+const email = ["thor@mail.com", "wolverine@gmail.com", "venom@gmail.com"];
+const password = ["thor01", "wolverine02", "venom03"];
+
+const users = [];
+
+for (let i = 0; names.length; i++) {
+  const user = {
+    name: names[i],
+    email: email[i],
+    password: password[i],
+  };
+  users.push(user);
+}
+console.log(users);
+
+function Login() {
+  var email = "thor@gmail.com".toLowerCase();
+  var password = "thor01";
+
+  const found = users.find(
+    (element) => element.email == email && element.password == password
+  );
+
+  if (found) {
+    alert("user found");
+  } else {
+    alert("user not found");
+  }
+}
+
+
+function Register() {
+  var email = "thor@gmail.com".toLowerCase()
+  var password = "test"
+  var name = "said"
+
+  const newUser = {
+    name,
+    email,
+    password
+  }
+
+  const found = users.find(element => element.email == email)
+}
